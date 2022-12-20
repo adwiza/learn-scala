4c0b8417a7d3e4c3e8532c8df8eff56ef9a89b28
Aleksei Elokhin <awiza@gmail.com>
2022-12-20 10:41:31 +0200

    functions added

--- src/main/scala/functions.scala
+++ src/main/scala/functions.scala
@@ -9,7 +9,22 @@ object functions {
   sum(1, 2)
   sum2(1, 2)
 
-  val list = List(sum2, sum2)
+//  val list = List(sum2, sum2)
 
-  list(0)(1, 2) // 3
+//  list(0)(1, 2) // 3
+  val list: List[(Int, Int) => Int] = List(sum _, sum _)
+
+  def bar(f: (Int, Int) => Int): Int = ???
+
+  bar(sum2)
+  bar(sum)
+
+  def isEven(v: Int): Boolean = v % 2 == 0
+
+  val xx = sum2
+  val xz: (Int, Int) => Int = sum
+  val xy: Int => Int => Int = xz.curried
+
+  val _: Int = xy(2)(2)
+
 }
