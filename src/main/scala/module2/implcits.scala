1a7da489f2e2dfa766981f4809270e08919db20a
Aleksei Elokhin <awiza@gmail.com>
2023-01-09 20:43:26 +0200

    9 lesson in progress

---
+++ src/main/scala/module2/implcits.scala
@@ -0,0 +1,42 @@
+package module2
+
+object implcits extends App {
+
+  // implicits conversions
+
+  /**
+   *
+   * Расширить возможности типа string, методом  trimToOption, который возповзает Option[String]
+   * если строка пустая иои null,  то None
+   * если нет. то Some от строки мо всеми начальными и конечными пробелами
+   */
+
+    implicit def stringToOps(str: String): StringOps = new StringOps(str)
+
+    class StringOps(string: String){
+      def trimToOption: Option[String] = Option(string).flatMap{ str =>
+        val trimed = str.trim
+        if(trimed.isEmpty) None
+        else Some(trimed)
+      }
+    }
+
+
+  val s1 = "FppBar".trimToOption
+  val s2 = "".trimToOption
+  val s3 = " ".trimToOption
+
+  // implicit conversions ОПАСНЫ
+
+//  implicit def strToUbt(str: String): Int = Integer.parseInt(str)
+
+  // "Foobar" / 2
+
+  implicit val seq = Seq("a", "b", "c") // val f: Int => String
+
+  def log(str: String): Unit = println(str)
+
+  log(str =  42)
+
+
+}
